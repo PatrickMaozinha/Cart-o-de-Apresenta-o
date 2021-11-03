90dc4d9c1e8df481557998eef27b272915cda3f0
PatrickMaozinha <patrick.maozinha@gmail.com>
2021-11-02 22:27:10 -0300

    https://github.com/PatrickMaozinha/cartao-de-apresentacao.git

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'PatrickMaozinha',
+  youtube: 'Rocketseat',
+  facebook: 'patrick.souza.1994',
+  instagram: 'maozinha013',
+  twitter: 'Patrick16520425'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
